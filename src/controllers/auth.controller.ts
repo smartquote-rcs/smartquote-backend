c2155d7f783ac5559f76c3386d8763e469bcfab3
alfredochivela <alfredochivela3@gmail.com>
2025-08-06 00:47:53 +0100

    add signIn and zod schema

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import supabase from '../infra/supabase/connect';
-import { signUpSchema } from '../schemas/auth.schema';
+import { signUpSchema } from '../schemas/signup.schema';
+import { signInSchema } from '../schemas/signIn.schema';
 
 class AuthController {
   async signUp(req: Request, res: Response): Promise<Response> {
@@ -31,6 +32,33 @@ class AuthController {
     return res.status(201).json({
       message: 'Usuário cadastrado com sucesso.',
       userId: data.user?.id,
+    });
+  }
+  async signIn(req: Request, res: Response): Promise<Response>
+  {
+      const parsed = signInSchema.safeParse(req.body);
+
+      if (!parsed.success) {
+        const errors = parsed.error.format();
+        return res.status(400).json({ errors });
+      }
+
+      const { email, password } = parsed.data;
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        return res.status(401).json({ error: error.message });
+      }
+
+    return res.status(200).json({
+      token: data.session?.access_token,
+      user: {
+        id: data.user.id,
+        email: data.user.email,
+      },
     });
   }
 }
--- src/routers/auth.route.ts
+++ src/routers/auth.route.ts
@@ -4,6 +4,6 @@ import AuthController from "../controllers/auth.controller";
 const authRouter = Router();
 
 authRouter.post("/signup",AuthController.signUp);
-authRouter.post("/sigin",AuthController.signUp);
+authRouter.post("/signin",AuthController.signIn);
 
 export default authRouter;

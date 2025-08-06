1c6e0693fd2737807c8afaaf2ff58ad3ca865c99
alfredochivela <alfredochivela3@gmail.com>
2025-08-06 23:46:21 +0100

    add middleware auth

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import authRouter from "./auth.route";
 import employeeRouter from "./employee.route";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const routers = Router();
 
 routers.use("/auth", authRouter);
-routers.use("/employee", employeeRouter);
+routers.use("/employee",authMiddleware, employeeRouter);
 export default routers;

0fab3d489e553b08bcb8a96cc9b800c042c8b82e
alfredo003 <alfredochivela3@gmail.com>
2025-08-07 11:36:31 +0100

    build doc of api

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,15 @@ dotenv.config();
 import express from "express";
 import routers from "./routers";
 import cors from "cors"
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocumentation from './swagger.json' assert {type:'json'};
+
 const port = process.env.PORT_DEFAULT || 2001
 const app = express();
 
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
+
+app.use(cors());
 app.use(express.json());
 app.use("/api",routers);
 

3f417426809348ede37c94f8d777f2f7b6bc5873
alfredochivela <alfredochivela3@gmail.com>
2025-08-11 18:54:33 +0100

    refactoring schemas zod

--- dist/schemas/Fornecedor.schema.d.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-export declare const fornecedorSchema: z.ZodObject<{
-    nome: z.ZodString;
-    email: z.ZodString;
-    telefone: z.ZodOptional<z.ZodString>;
-    endereco: z.ZodOptional<z.ZodString>;
-}, z.core.$strip>;
-//# sourceMappingURL=Fornecedor.schema.d.ts.map
--- dist/schemas/produto.schema.d.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-export declare const produtoSchema: z.ZodObject<{
-    nome: z.ZodString;
-    preco: z.ZodNumber;
-    estoque: z.ZodNumber;
-    descricao: z.ZodOptional<z.ZodString>;
-}, z.core.$strip>;
-//# sourceMappingURL=produto.schema.d.ts.map

6437f161fa2e6b1d634a3eedbd4f20c3e733ad91
alfredocumaio <alfredodejesus67@hotmail.com>
2025-08-14 12:23:18 +0100

    aa

--- src/schemas/CotacaoSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const cotacaoSchema = z.object({
-  prompt_id: z.number().int().positive(),
-  produto_id: z.number().int().positive(),
-  preco: z.number().positive(),
-  observacao: z.string().optional(),
-});
